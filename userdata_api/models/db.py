44ac986ef88bfefff932d3159e06832c0f2309dc
semen603089 <semen603089@mail.ru>
2023-05-14 17:25:35 +0300

    docs

--- userdata_api/models/db.py
+++ userdata_api/models/db.py
@@ -14,12 +14,25 @@ from userdata_api.models.base import BaseDbModel
 
 
 class ViewType(str, Enum):
+    """
+    Тип отображения пользоватльских данных в ответе `GET /user/{user_id}`
+    ALL: {category: {param: [val1, val2, ...]}}
+    LAST: {category: {param: last_modified_value}}
+    MOST_TRUSTED: {category: {param: most_trusted_value}}
+    """
+
     ALL: Final[str] = "all"
     LAST: Final[str] = "last"
     MOST_TRUSTED: Final[str] = "most_trusted"
 
 
 class Category(BaseDbModel):
+    """
+    Категория - объеденение параметров пользовательских данных.
+    Если параметром может быть, например, номер студенческого и номер профсоюзного,
+    то категорией, их объединяющей, может быть "студенческая информация" или "документы"
+    """
+
     name: Mapped[str] = mapped_column(String)
     read_scope: Mapped[str] = mapped_column(String, nullable=True)
     update_scope: Mapped[str] = mapped_column(String, nullable=True)
@@ -37,6 +50,13 @@ class Category(BaseDbModel):
 
 
 class Param(BaseDbModel):
+    """
+    Параметр - находится внутри категории,
+    к нему можно задавать значение у конкретного пользователя.
+    Например, параметрами может являться почта и номер телефона,
+    а параметры эти могут лежать в категории "контакты"
+    """
+
     name: Mapped[str] = mapped_column(String)
     category_id: Mapped[int] = mapped_column(Integer, ForeignKey(Category.id))
     is_required: Mapped[bool] = mapped_column(Boolean, default=False)
@@ -68,6 +88,13 @@ class Param(BaseDbModel):
 
 
 class Source(BaseDbModel):
+    """
+    Источник данных - субъект изменения польщовательских данных - тот, кто меняет данные
+    В HTTP методах доступно только два источника - user/admin
+    Субъект может менять только данные, созданные собой же.
+    У источника есть уровень доверия, который влияет на вид ответа `GET /user/{user_id}`
+    """
+
     name: Mapped[str] = mapped_column(String, unique=True)
     trust_level: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
     create_ts: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
@@ -84,6 +111,12 @@ class Source(BaseDbModel):
 
 
 class Info(BaseDbModel):
+    """
+    Значения параметров для конкретных польщзователей
+    Если, например, телефон - параметр, то здесь указывается его значение для
+    польщзователя(owner_id) - объекта изменения пользовательских данных
+    """
+
     param_id: Mapped[int] = mapped_column(Integer, ForeignKey(Param.id))
     source_id: Mapped[int] = mapped_column(Integer, ForeignKey(Source.id))
     owner_id: Mapped[int] = mapped_column(Integer, nullable=False)
