5cf1760eac1b42a023fcd6d9794f65e05aca1fa7
Semyon Grigoriev <s.grigoriev@cdnvideo.com>
2023-08-11 15:59:04 +0300

    cicd, fixes

--- worker/backends/kafka.py
+++ worker/backends/kafka.py
@@ -1,8 +1,7 @@
 import json
 import logging
-from pprint import pformat
 
-from confluent_kafka import Consumer, Message
+from confluent_kafka import Consumer, Message, TopicPartition
 from event_schema.auth import UserLogin, UserLoginKey
 from pydantic import ValidationError
 
@@ -10,8 +9,6 @@ from settings import get_settings
 from userdata_api import __version__
 
 from .kafkameta import KafkaMeta
-from .pg import PgSession
-
 
 log = logging.getLogger(__name__)
 
@@ -24,7 +21,6 @@ class KafkaConsumer(KafkaMeta):
     __password: str | None = get_settings().KAFKA_PASSWORD
     __group_id: str | None = get_settings().KAFKA_GROUP_ID
     __topics: list[str] = get_settings().KAFKA_TOPICS
-    _pg = PgSession()
     _consumer: Consumer
 
     def __configurate(self) -> None:
@@ -59,16 +55,18 @@ class KafkaConsumer(KafkaMeta):
     @staticmethod
     def _stats_cb(stats_json_str: str):
         stats_json = json.loads(stats_json_str)
-        log.info('\nKAFKA Stats: {}\n'.format(pformat(stats_json)))
+        log.info('\nKAFKA Stats: {}\n'.format(stats_json))
 
     @staticmethod
     def _on_assign(consumer, partitions):
         log.info(f'Assignment: {partitions}')
 
+
     def __init__(self):
         self.__configurate()
         self._consumer = Consumer(self.__conf)
         self._consumer.subscribe(["test-user-login"], on_assign=KafkaConsumer._on_assign)
+
 
     def _parse_message(self, msg: Message) -> tuple[int, UserLogin]:
         user_info = UserLogin.model_validate(json.loads(msg.value()))
@@ -77,14 +75,13 @@ class KafkaConsumer(KafkaMeta):
 
     def close(self):
         self._consumer.close()
-        self._pg.close()
 
     def run(self) -> None:
         try:
             while True:
                 msg = self._consumer.poll(timeout=1.0)
-                print(msg)
                 if msg is None:
+                    log.debug("Message is None")
                     continue
                 if msg.error():
                     log.error(f"Message {msg=} reading triggered: {msg.error()}, Retrying...")
@@ -92,12 +89,12 @@ class KafkaConsumer(KafkaMeta):
                 log.info('%% %s [%d] at offset %d\n' % (msg.topic(), msg.partition(), msg.offset()))
                 try:
                     user_id, user_info = self._parse_message(msg)
-                    print(user_id, user_info)
-                except ValidationError:
+                except (ValidationError, json.JSONDecodeError):
                     log.error(f"Message skipped due to validation error", exc_info=True)
                     self._consumer.store_offsets(msg)
                     continue
                 self._patch_user_info(user_info, user_id)
                 self._consumer.store_offsets(msg)
         finally:
+            log.info("Consumer closed")
             self.close()
