affcddcfd480b13510333db5815415a173f4606e
Zimovchik <zimovchik@outlook.com>
2024-07-29 12:09:46 +0300

    Update test_users_get.py

---
+++ tests/test_routes/test_users_get.py
@@ -0,0 +1,146 @@
+from time import sleep
+
+import pytest
+
+from userdata_api.models.db import *
+from userdata_api.utils.utils import random_string
+
+
+@pytest.mark.authenticated("userdata.info.admin")
+def test_get(client, dbsession, category_no_scopes, source):
+    source = source()
+    category1 = category_no_scopes()
+    category2 = category_no_scopes()
+    category3 = category_no_scopes()
+    param1 = Param(
+        name=f"test{random_string()}", category_id=category1.id, type="last", changeable=True, is_required=True
+    )
+    param2 = Param(
+        name=f"test{random_string()}", category_id=category1.id, type="last", changeable=True, is_required=True
+    )
+    param3 = Param(
+        name=f"test{random_string()}", category_id=category2.id, type="last", changeable=True, is_required=True
+    )
+    param4 = Param(
+        name=f"test{random_string()}", category_id=category3.id, type="last", changeable=True, is_required=True
+    )
+    dbsession.add_all([param1, param2, param3, param4])
+    dbsession.flush()
+    info1 = Info(value=f"test{random_string()}", source_id=source.id, param_id=param1.id, owner_id=0)
+    info2 = Info(value=f"test{random_string()}", source_id=source.id, param_id=param2.id, owner_id=1)
+    info3 = Info(value=f"test{random_string()}", source_id=source.id, param_id=param3.id, owner_id=0)
+    info4 = Info(value=f"test{random_string()}", source_id=source.id, param_id=param4.id, owner_id=1)
+    dbsession.add_all([info1, info2, info3, info4])
+    dbsession.commit()
+    response = client.get(f"/user", params={"users": [0, 1], "categories": [category1.id, category2.id, category3.id]})
+    assert response.status_code == 200
+    assert {"category": category1.name, "param": info2.param.name, "value": info2.value} in list(
+        response.json()["users"]["1"]["items"]
+    )
+    assert {"category": category3.name, "param": info4.param.name, "value": info4.value} in list(
+        response.json()["users"]["1"]["items"]
+    )
+    assert {"category": category1.name, "param": info1.param.name, "value": info1.value} in list(
+        response.json()["users"]["0"]["items"]
+    )
+    assert {"category": category2.name, "param": info3.param.name, "value": info3.value} in list(
+        response.json()["users"]["0"]["items"]
+    )
+    dbsession.delete(info1)
+    dbsession.delete(info2)
+    dbsession.delete(info3)
+    dbsession.delete(info4)
+    dbsession.flush()
+    dbsession.delete(param1)
+    dbsession.delete(param2)
+    dbsession.delete(param3)
+    dbsession.delete(param4)
+    dbsession.commit()
+
+
+@pytest.mark.authenticated("userdata.info.admin")
+def test_get_some_users(client, dbsession, category_no_scopes, source):
+    source = source()
+    category1 = category_no_scopes()
+    param1 = Param(
+        name=f"test{random_string()}", category_id=category1.id, type="last", changeable=True, is_required=True
+    )
+    dbsession.add_all([param1])
+    dbsession.flush()
+    info1 = Info(value=f"test{random_string()}", source_id=source.id, param_id=param1.id, owner_id=1)
+    info2 = Info(value=f"test{random_string()}", source_id=source.id, param_id=param1.id, owner_id=2)
+    info3 = Info(value=f"test{random_string()}", source_id=source.id, param_id=param1.id, owner_id=3)
+    dbsession.add_all([info1, info2, info3])
+    dbsession.commit()
+    response = client.get(f"/user", params={"users": [1, 2], "categories": [category1.id]})
+    assert response.status_code == 200
+    assert len(response.json()["users"]) == 2
+    assert {"category": category1.name, "param": param1.name, "value": info1.value} in list(
+        response.json()["users"]["1"]["items"]
+    )
+    assert {"category": category1.name, "param": param1.name, "value": info2.value} in list(
+        response.json()["users"]["2"]["items"]
+    )
+    assert not "3" in list(response.json()["users"].keys())
+    response = client.get(f"/user", params={"users": [3], "categories": [category1.id]})
+    assert response.status_code == 200
+    assert len(response.json()["users"]) == 1
+    assert {"category": category1.name, "param": param1.name, "value": info3.value} in list(
+        response.json()["users"]["3"]["items"]
+    )
+    dbsession.delete(info1)
+    dbsession.delete(info2)
+    dbsession.delete(info3)
+    dbsession.flush()
+    dbsession.delete(param1)
+    dbsession.commit()
+
+
+@pytest.mark.authenticated("userdata.info.admin")
+def test_get_some_categories(client, dbsession, category_no_scopes, source):
+    source = source()
+    category1 = category_no_scopes()
+    category2 = category_no_scopes()
+    category3 = category_no_scopes()
+    param1 = Param(
+        name=f"test{random_string()}", category_id=category1.id, type="last", changeable=True, is_required=True
+    )
+    param2 = Param(
+        name=f"test{random_string()}", category_id=category2.id, type="last", changeable=True, is_required=True
+    )
+    param3 = Param(
+        name=f"test{random_string()}", category_id=category3.id, type="last", changeable=True, is_required=True
+    )
+    dbsession.add_all([param1, param2, param3])
+    dbsession.flush()
+    info1 = Info(value=f"test{random_string()}", source_id=source.id, param_id=param1.id, owner_id=1)
+    info2 = Info(value=f"test{random_string()}", source_id=source.id, param_id=param2.id, owner_id=1)
+    info3 = Info(value=f"test{random_string()}", source_id=source.id, param_id=param3.id, owner_id=1)
+    dbsession.add_all([info1, info2, info3])
+    dbsession.commit()
+    response = client.get(f"/user", params={"users": [1], "categories": [category1.id, category2.id]})
+    assert response.status_code == 200
+    assert {"category": category1.name, "param": info1.param.name, "value": info1.value} in list(
+        response.json()["users"]["1"]["items"]
+    )
+    assert {"category": category2.name, "param": info2.param.name, "value": info2.value} in list(
+        response.json()["users"]["1"]["items"]
+    )
+    assert not (
+        {"category": category3.name, "param": info3.param.name, "value": info3.value}
+        in list(response.json()["users"]["1"]["items"])
+    )
+
+    response = client.get(f"/user", params={"users": [1], "categories": [category3.id]})
+    assert {"category": category3.name, "param": info3.param.name, "value": info3.value} in list(
+        response.json()["users"]["1"]["items"]
+    )
+
+    dbsession.delete(info1)
+    dbsession.delete(info2)
+    dbsession.delete(info3)
+    dbsession.flush()
+    dbsession.delete(param1)
+    dbsession.delete(param2)
+    dbsession.delete(param3)
+    dbsession.commit()
