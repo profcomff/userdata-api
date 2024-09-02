1ed868d776fad57b406a7591a153fa6db92875f5
gitfresnel <151745312+gitfresnel@users.noreply.github.com>
2024-09-02 08:20:32 +0300

    f

---
+++ migrations/versions/665f07866b38_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 665f07866b38
+Revises: ea9459426db1
+Create Date: 2024-09-02 08:20:08.846764
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '665f07866b38'
+down_revision = 'ea9459426db1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
