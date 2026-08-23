27c95f2effff5c92d6ceb3592406fc81fd6f1bca
Timur Enikeev <enikeevt04@gmail.com>
2026-08-23 12:28:08 -0400

    Remove category from Alias

--- userdata_api/routes/param_alias.py
+++ userdata_api/routes/param_alias.py
@@ -12,31 +12,23 @@ from userdata_api.schemas.param_alias import ParamAliasGet, ParamAliasPatch, Par
 from userdata_api.schemas.response_model import StatusResponseModel
 
 
-param_alias = APIRouter(prefix="/category/{category_id}/param/{param_id}/alias", tags=["Param Alias"])
+param_alias = APIRouter(prefix="/param/{param_id}/alias", tags=["Param Alias"])
 
 
-def _get_param(*, category_id: int, param_id: int) -> Param:
-    param = (
-        Param.query(session=db.session)
-        .filter(
-            Param.id == param_id,
-            Param.category_id == category_id,
-        )
-        .one_or_none()
-    )
+def _get_param(*, param_id: int) -> Param:
+    param = Param.query(session=db.session).filter(Param.id == param_id).one_or_none()
     if not param:
         raise ObjectNotFound(Param, param_id)
     return param
 
 
-def _get_param_alias(*, category_id: int, param_id: int, alias_id: int) -> ParamAlias:
+def _get_param_alias(*, param_id: int, alias_id: int) -> ParamAlias:
     alias = (
         ParamAlias.query(session=db.session)
         .join(Param)
         .filter(
             ParamAlias.id == alias_id,
             ParamAlias.param_id == param_id,
-            Param.category_id == category_id,
             not_(Param.is_deleted),
         )
         .one_or_none()
@@ -65,7 +57,6 @@ def _check_alias_name_exists(name: str, *, alias_id: int | None = None) -> None:
 @param_alias.post("", response_model=ParamAliasGet)
 async def create_param_alias(
     request: Request,
-    category_id: int,
     param_id: int,
     alias_inp: ParamAliasPost,
     _: dict[str, Any] = Depends(UnionAuth(scopes=["userdata.param.create"], allow_none=False, auto_error=True)),
@@ -73,7 +64,7 @@ async def create_param_alias(
     """
     Создать алиас параметра.
     """
-    _ = _get_param(category_id=category_id, param_id=param_id)
+    _ = _get_param(param_id=param_id)
     _validate_source(alias_inp.source_id)
     _check_alias_name_exists(alias_inp.name)
     alias = ParamAlias.create(session=db.session, param_id=param_id, **alias_inp.model_dump())
@@ -82,23 +73,22 @@ async def create_param_alias(
 
 @param_alias.get("/{alias_id}", response_model=ParamAliasGet)
 async def get_param_alias(
-    category_id: int,
     param_id: int,
     alias_id: int,
 ) -> ParamAliasGet:
     """
     Получить алиас параметра по айди.
     """
-    alias = _get_param_alias(category_id=category_id, param_id=param_id, alias_id=alias_id)
+    alias = _get_param_alias(param_id=param_id, alias_id=alias_id)
     return ParamAliasGet.model_validate(alias)
 
 
 @param_alias.get("", response_model=list[ParamAliasGet])
-async def get_param_aliases(category_id: int, param_id: int) -> list[ParamAliasGet]:
+async def get_param_aliases(param_id: int) -> list[ParamAliasGet]:
     """
     Получить все алиасы параметра.
     """
-    _ = _get_param(category_id=category_id, param_id=param_id)
+    _ = _get_param(param_id=param_id)
     aliases = ParamAlias.query(session=db.session).filter(ParamAlias.param_id == param_id).all()
     type_adapter = TypeAdapter(list[ParamAliasGet])
     return type_adapter.validate_python(aliases)
@@ -106,7 +96,6 @@ async def get_param_aliases(category_id: int, param_id: int) -> list[ParamAliasG
 
 @param_alias.patch("/{alias_id}", response_model=ParamAliasGet)
 async def patch_param_alias(
-    category_id: int,
     param_id: int,
     alias_id: int,
     alias_inp: ParamAliasPatch,
@@ -115,7 +104,7 @@ async def patch_param_alias(
     """
     Обновить алиас параметра.
     """
-    alias = _get_param_alias(category_id=category_id, param_id=param_id, alias_id=alias_id)
+    alias = _get_param_alias(param_id=param_id, alias_id=alias_id)
     patch_data = alias_inp.model_dump(exclude_unset=True)
     if "name" in patch_data:
         _check_alias_name_exists(patch_data["name"], alias_id=alias.id)
@@ -128,7 +117,6 @@ async def patch_param_alias(
 @param_alias.delete("/{alias_id}", response_model=StatusResponseModel)
 async def delete_param_alias(
     request: Request,
-    category_id: int,
     param_id: int,
     alias_id: int,
     _: dict[str, Any] = Depends(UnionAuth(scopes=["userdata.param.delete"], allow_none=False, auto_error=True)),
@@ -136,6 +124,6 @@ async def delete_param_alias(
     """
     Удалить алиас параметра.
     """
-    _ = _get_param_alias(category_id=category_id, param_id=param_id, alias_id=alias_id)
+    _ = _get_param_alias(param_id=param_id, alias_id=alias_id)
     ParamAlias.delete(alias_id, session=db.session)
     return StatusResponseModel(status="Success", message="Param alias deleted", ru="Алиас параметра удален")
